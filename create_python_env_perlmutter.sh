ba02932106b9548f4aa8fc248eee7a6c73d4fffe
adityaranjan <adi.k.ranjan@gmail.com>
2025-05-02 15:41:43 -0400

    add env creation scripts

---
+++ create_python_env_perlmutter.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+WRKSPC=$SCRATCH
+# everything will be installed in $WRKSPC
+
+ENV_NAME="my-venv"
+# this is the name of your python venv, change if needed
+
+cd $WRKSPC
+echo -e "${RED}Creating Python Environment in $WRKSPC:${GREEN}"
+module load python 
+python -m venv $WRKSPC/$ENV_NAME 
+module unload python
+
+echo -e "${RED}Installing Dependencies:${GREEN}"
+#Step 1 - activate your venv
+source $WRKSPC/$ENV_NAME/bin/activate
+
+
+#Step 2 - install torch
+pip install --upgrade pip
+pip install axonn
+pip install numpy
+pip install ogb
+pip install torch
+pip install axonn numpy ogb torch torch_geometric
+
+
+echo -e "${RED}Your Python Environment is ready. To activate it run the following commands in the SAME order:${NC}"
+echo -e "${GREEN}source $WRKSPC/$ENV_NAME/bin/activate${NC}"
+echo ""
+echo -e "${NC}"
